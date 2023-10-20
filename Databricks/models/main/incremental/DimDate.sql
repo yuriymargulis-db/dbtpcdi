5931d76c24b2b06e9ceb85e81ff4b577260e6437
yuriymargulis-db <132605381+yuriymargulis-db@users.noreply.github.com>
2023-10-20 02:13:22 -0700

    Final

--- Databricks/models/main/incremental/DimDate.sql
+++ Databricks/models/main/incremental/DimDate.sql
@@ -4,24 +4,7 @@
     )
 }}
 
-select col_name
-      ,sk_dateid
-      ,datevalue
-      ,datedesc
-      ,calendaryearid
-      ,calendaryeardesc
-      ,calendarqtrid
-      ,calendarqtrdesc
-      ,calendarmonthid
-      ,calendarmonthdesc
-      ,calendarweekid
-      ,calendarweekdesc
-      ,dayofweeknum
-      ,dayofweekdesc
-      ,fiscalyearid
-      ,fiscalyeardesc
-      ,fiscalqtrid
-      ,fiscalqtrdesc
-      ,holidayflag
+select 
+   *
 from
     {{ source('tpcdi', 'DimDate') }}
